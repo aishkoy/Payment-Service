21645c9d57b34b97bee8e693fd145f042948f721
aishkou <a.orozbekovaa@gmail.com>
2025-04-05 12:06:01 +0600

    feat: configured Spring Security

---
+++ src/main/java/kg/attractor/payment/config/SecurityConfig.java
@@ -0,0 +1,68 @@
+package kg.attractor.payment.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final DataSource dataSource;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        String fetchUser = "select EMAIL, PASSWORD, ENABLED from USERS where EMAIL = ?;";
+
+        String fetchRole = """
+                select u.EMAIL as EMAIL, concat('ROLE_', r.ROLE) as ROLE
+                from USERS u, ROLES r
+                where u.EMAIL = ?
+                and u.ROLE_ID = r.ID;
+                """;
+
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery(fetchUser)
+                .authoritiesByUsernameQuery(fetchRole);
+
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery(fetchUser)
+                .authoritiesByUsernameQuery(fetchRole);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(AbstractHttpConfigurer::disable)
+                .logout(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                        .requestMatchers("/api/register").permitAll()
+                        .requestMatchers("/api/admin").hasRole("ADMIN")
+                        .anyRequest().hasRole("USER")
+                );
+        return http.build();
+    }
+}
